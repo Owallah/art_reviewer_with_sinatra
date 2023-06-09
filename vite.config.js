2b43899a6f1404ff66088d4ed869db8d4c5d142f
Owallah <mikesonowallah@gmail.com>
2023-06-09 15:56:40 +0300

    updated vite config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/Owallah/art_reviewer_with_sinatra",
+  base: "/art_reviewer_with_sinatra",
   plugins: [react()],
 })
